ae4be34858d7ec236553043b6a9545ff592afafa
audreyfogle <FogleA10@gmail.com>
2022-05-05 22:02:45 -0400

    test git commit -m test

--- __test__/random.test.js
+++
@@ -1,6 +0,0 @@
-const { exportAllDeclaration } = require('@babel/types');
-const { default: TestRunner } = require('jest-runner');
-const checkIfEqual = require('../lib/random.js');
-test('checks if 10 is equal to 10',() => {
-    exportAllDeclaration(checkIfEqual(10, 10)).toBe(true);
-});
