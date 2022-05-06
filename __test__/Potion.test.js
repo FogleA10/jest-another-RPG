fe9c876057bed8ec51bb2e319fec90ecacac03fa
audreyfogle <FogleA10@gmail.com>
2022-05-06 14:35:24 -0400

    more js

--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -6,3 +6,12 @@ test('creates a health potion object', () => {
     expect(potion.name).toBe('health');
     expect(potion.value).toEqual(expect.any(Number));
   });
+
+  function Car(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+  new Car('Honda', 'Civic');
+
+  const car = new Car('Honda', 'Civic');
+console.log(car);
