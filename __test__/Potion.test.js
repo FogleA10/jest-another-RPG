3001e6c0c66782a46925d5393fe7f321ee592082
audreyfogle <FogleA10@gmail.com>
2022-05-06 22:22:32 -0400

    test issues

--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -1,9 +1,10 @@
 const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object', () => {
-    const potion = new Potion('health');
+    const potion = new Potion();
   
-    expect(potion.name).toBe('health');
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
   });
 
@@ -15,3 +16,16 @@ test('creates a health potion object', () => {
 
   const car = new Car('Honda', 'Civic');
 console.log(car);
+
+function Potion(name) {
+    this.types = ['strength', 'agility', 'health'];
+    this.name = name;
+  
+    if (this.name === 'health') {
+      this.value = Math.floor(Math.random() * 10 + 30);
+    } else {
+      this.value = Math.floor(Math.random() * 5 + 7);
+    }
+  }
+  
+  module.exports = Potion;
